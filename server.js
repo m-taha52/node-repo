a485ca8f961489b858c49e5d47f4992a05ebe6cf
mohammed taha <mohammed@pop-os.localdomain>
2021-11-28 17:21:17 +0300

    express continued

--- server.js
+++ server.js
@@ -4,12 +4,36 @@ const app = express()
 
 const PORT = 3000;
 
+const friends = [{
+        id: 0,
+        name: "Ahmed",
+
+    },
+    {
+        id: 1,
+        name: "Ali",
+
+    },
+    {
+        id: 2,
+        name: "Sarah",
+
+    }
+]
+
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
 })
 
-app.get("/", (req, res) => {
-    res.send("hellooooooo")
+app.get("/friends", (req, res) => {
+    res.json(friends)
+})
+
+app.get("/friends/:friendId", (req, res) => {
+    const friendId = Number(req.params.friendId)
+    console.log(friendId)
+    const friend = friends[friendId]
+    res.json(friend)
 })
 
 app.get("/messages", (req, res) => {
@@ -24,3 +48,6 @@ app.post("/messages", (req, res) => {
 
 
 //naming the file server.js makes it run automatically without changing package.json
+
+//npm install <name of package> --save -dev is used to install the package in the dev dependencies section in package.json
+//devdependencies are packages that are not absolutely required to run
